65c0d9870a564dea90166b379604d4c2bf49b659
Yuvv <yuvvyuvv@outlook.com>
2022-07-22 00:12:02 +0800

    220722

--- 0701-0800/0792-number-of-matching-subsequences.java
+++ 0701-0800/0792-number-of-matching-subsequences.java
@@ -9,23 +9,24 @@ import java.util.*;
 public class Solution {
     private int[][] buildNextTable(String s) {
         // last row means first position
-        int[][] nextTable = new int[s.length() + 1][26];
-        for (int i = 0; i < s.length(); i++) {
-            java.util.Arrays.fill(nextTable[i], -1);
-        }
+        int[][] nextTable = new int[s.length() + 1][];
+        int[] row = new int[26];
+        Arrays.fill(row, -1);
+        nextTable[s.length() - 1] = Arrays.copyOf(row, 26);
 
         for (int i = s.length() - 1; i >= 0; i--) {
             int val = s.charAt(i) - 'a';
-            for (int j = 0; j < i; j++) {
-                nextTable[j][val] = i;
+            row[val] = i;
+            if (i > 0) {
+                nextTable[i - 1]= Arrays.copyOf(row, 26);
             }
-            nextTable[s.length()][val] = i;
         }
+        nextTable[s.length()] = row;
 
         return nextTable;
     }
 
-    public int numMatchingSubseq_tle(String s, String[] words) {
+    public int numMatchingSubseq(String s, String[] words) {
         int[][] nextTable = buildNextTable(s);
         int count = 0;
         for (String word : words) {
@@ -48,7 +49,8 @@ public class Solution {
         return count;
     }
 
-    public int numMatchingSubseq(String s, String[] words)  {
+    // a little slow, but AC
+    public int numMatchingSubseq_hash(String s, String[] words) {
         Map<Character, TreeSet<Integer>> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
@@ -83,11 +85,11 @@ public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
         // 2
-        System.out.println(s.numMatchingSubseq(
+        System.out.println(s.numMatchingSubseq_tle(
             "dsahjpjauf", new String[] {"ahjpjau", "ja", "ahbwzgqnuk", "tnmlanowax"}
         ));
         // 3
-        System.out.println(s.numMatchingSubseq(
+        System.out.println(s.numMatchingSubseq_tle(
             "abcde", new String[] {"a", "bb", "acd", "ace"}
         ));
     }
